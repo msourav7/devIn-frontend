eb7f608656434a8240144a31667293cbec360944
Sourav Mishra <msourav.4411@gmail.com>
2025-03-06 22:23:41 +0530

    Restructured some files into components folder

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,9 @@
 import axios from "axios";
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
-import { addUser } from "./utils/userSlice";
+import { addUser } from "../utils/userSlice";
 import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
 
 const Login = () => {
   const [emailId, setEmailId] = useState("");
@@ -13,7 +14,7 @@ const Login = () => {
   const handleLogin = async () => {
     try {
       const res = await axios.post(
-        "http://localhost:7777/login",
+        BASE_URL + "/login",
         {
           emailId,
           password,
