d25dfef2f2fda19b8be25e780e338f4c04162ec0
Sourav Mishra <msourav.4411@gmail.com>
2025-04-20 03:19:10 +0530

    css

--- src/components/Connections.jsx
+++ src/components/Connections.jsx
@@ -56,44 +56,49 @@ const Connections = () => {
   // );
 
   return (
-    <div className="text-center my-10">
-      <h1 className="font-bold text-white text-3xl">Connections</h1>
+    <>
+      <div className="pb-24">
+        <div className="text-center my-10">
+          <h1 className="font-bold text-white text-3xl">Connections</h1>
 
-      {connections.map((e) => {
-        const { _id, firstName, lastName, age, gender, about, photoUrl } = e;
+          {connections.map((e) => {
+            const { _id, firstName, lastName, age, gender, about, photoUrl } =
+              e;
 
-        return (
-          <div
-            key={_id}
-            className="flex flex-col md:flex-row md:items-center m-4 p-4 rounded-lg bg-base-300 w-11/12 md:w-1/2 mx-auto"
-          >
-            {/* Group photo + bio in a row always */}
-            <div className="flex items-center">
-              <img
-                alt="photo"
-                className="w-20 h-20 rounded-full object-center"
-                src={photoUrl}
-              />
-              <div className="text-left mx-4">
-                {lastName && (
-                  <h2 className="font-bold text-lg">
-                    {firstName + " " + lastName}
-                  </h2>
-                )}
-                <p>{age + " " + gender}</p>
-                <p>{about}</p>
+            return (
+              <div
+                key={_id}
+                className="flex flex-col md:flex-row md:items-center m-4 p-4 rounded-lg bg-base-300 w-11/12 md:w-1/2 mx-auto"
+              >
+                {/* Group photo + bio in a row always */}
+                <div className="flex items-center">
+                  <img
+                    alt="photo"
+                    className="w-20 h-20 rounded-full object-center"
+                    src={photoUrl}
+                  />
+                  <div className="text-left mx-4">
+                    {lastName && (
+                      <h2 className="font-bold text-lg">
+                        {firstName + " " + lastName}
+                      </h2>
+                    )}
+                    <p>{age + " " + gender}</p>
+                    <p>{about}</p>
+                  </div>
+                </div>
+
+                <Link to={"/chat/" + _id}>
+                  <button className="btn btn-primary mt-4 md:mt-0 mx-auto md:mx-0 md:ml-auto self-start md:self-center">
+                    Chat
+                  </button>
+                </Link>
               </div>
-            </div>
-
-            <Link to={"/chat/"+_id}>
-              <button className="btn btn-primary mt-4 md:mt-0 mx-auto md:mx-0 md:ml-auto self-start md:self-center">
-                Chat
-              </button>
-            </Link>
-          </div>
-        );
-      })} 
-    </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
   );
 };
 
